c175742b347e42a0c82c9ab82b08e5d2a2e6217b
unknown <nenad.slavujevic@posteo.de>
2019-01-26 00:31:51 +0100

    random player color

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChanger : MonoBehaviour
+{
+    Image image;
+
+    public GameObject go;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void ChangeColor()
+    {
+        Color newColor = new Color(Random.Range(0f,1f), Random.Range(0f,1f), Random.Range(0f,1f));
+        go.GetComponent<Renderer>().material.color = newColor;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Renderer cloth1;
+    [SerializeField] Renderer cloth2;
+    [SerializeField] Renderer cloth3;
+
     private Rigidbody rb;
+
     [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        cloth1.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        cloth2.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        cloth3.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
     }
 
     private void FixedUpdate()
@@ -17,10 +29,13 @@ public class PlayerController : MonoBehaviour
         float horizontalValue = Input.GetAxis("Horizontal");
         float verticalValue = Input.GetAxis("Vertical");
 
-        if(verticalValue != 0 || horizontalValue != 0)
+        Vector3 moveDirection = new Vector3(horizontalValue, 0f, verticalValue);
+
+        if (verticalValue != 0 || horizontalValue != 0)
         {
-            transform.Translate(new Vector3(horizontalValue, 0f, verticalValue) * moveSpeed, Space.World);
-            //rb.AddForce(new Vector3(horizontalValue, verticalValue, 0f) * moveSpeed);
+            rb.velocity = moveDirection * moveSpeed;
         }
+
+        transform.LookAt(transform.position + moveDirection);
     }
 }
