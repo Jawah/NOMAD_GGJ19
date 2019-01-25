18f3f2833ffc78b9ec2bf321671499f53febe2f3
unknown <nenad.slavujevic@posteo.de>
2019-01-25 22:54:21 +0100

    added tile and player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float moveSpeed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalValue = Input.GetAxis("Horizontal");
+        float verticalValue = Input.GetAxis("Vertical");
+
+        if(verticalValue != 0 || horizontalValue != 0)
+        {
+            transform.Translate(new Vector3(horizontalValue, 0f, verticalValue) * moveSpeed, Space.World);
+            //rb.AddForce(new Vector3(horizontalValue, verticalValue, 0f) * moveSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/TileMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMover : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.back * moveSpeed);
+    }
+}
