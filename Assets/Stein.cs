22c9b43b5f65a19367e95fd3e985d7b0859b0d9c
Konrad Mampe <konradmampe@Konrads-MacBook-Pro.local>
2019-01-26 21:01:11 +0100

    Stoner done

---
+++ Assets/Stein.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Stein : MonoBehaviour
+{
+
+    public GameObject middlePoint;
+    public GameObject endPoint;
+
+    public bool middlePointReached = false;
+    public bool endPointReached = false;
+
+    public int amountOfPlayers;
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        if (amountOfPlayers == 1 && !middlePointReached)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, middlePoint.transform.position, speed);
+        }
+
+        if (Vector3.Distance(transform.position, middlePoint.transform.position) < 0.1f && !middlePointReached)
+        {
+            transform.position = middlePoint.transform.position;
+            middlePointReached = true;
+        }
+
+        if (middlePointReached && !endPointReached)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position, speed);
+        }
+
+        if (Vector3.Distance(transform.position, endPoint.transform.position) < 0.1f && !endPointReached)
+        {
+            transform.position = endPoint.transform.position;
+            endPointReached = true;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            amountOfPlayers++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            amountOfPlayers--;
+        }
+    }
+
+}
